c9642922b9ab58bcf8844b36580d49afbb6a16a8
PomatoCoCEO <paulomiguelc@gmail.com>
2023-02-20 14:29:42 +0000

    DEV: add edge detection with Sobel filter and NMS

--- PL1/matlab/Assignment.m
+++ PL1/matlab/Assignment.m
@@ -1,11 +1,24 @@
-function [img, filter_avg, img_filter_implemented, img_filter_matlab] = Assignment()
+function [img, filter_avg, img_avg_implemented, img_avg_matlab] = Assignment()
+ % this part can be turned into a cycle for the testing of multiple kinds
+ % of filters. Still it appears to be working for both cases.
     img = double(rgb2gray(imread("../data/img00.jpg")));
     % test thing with sobel filter
-    filter_avg = fspecial("sobel");
-    disp('starting implemented filter passing...');
-    img_filter_implemented = ImageFilter(img,filter_avg);
-    disp('starting MATLAB filter passing...');
-    img_filter_matlab = imfilter(img,filter_avg,'replicate','conv','same');
-    fprintf("max difference detected: %d\n", max(max(abs(img_filter_matlab- img_filter_implemented))));
-    fprintf("Example: first element of both: %f and %f\n",img_filter_implemented(1,1), img_filter_matlab(1,1));
+    filter_sobel = fspecial("sobel");
+    disp('sobel - starting implemented filter passing...');
+    img_sobel_implemented = ImageFilter(img,filter_sobel);
+    disp('sobel - starting MATLAB filter passing...');
+    img_sobel_matlab = imfilter(img,filter_sobel,'replicate','conv','same');
+    
+    fprintf("sobel - max difference detected: %d\n", max(max(abs(img_sobel_matlab- img_sobel_implemented))));
+    fprintf("sobel - Example: first element of both: %f and %f\n",img_sobel_implemented(1,1), img_sobel_matlab(1,1));
+
+    filter_avg = fspecial("average",7);
+    disp('average - starting implemented filter passing...');
+    img_avg_implemented = ImageFilter(img,filter_avg);
+    disp('average - starting MATLAB filter passing...');
+    img_avg_matlab = imfilter(img,filter_avg,'replicate','conv','same');
+
+
+    fprintf("average - max difference detected: %d\n", max(max(abs(img_avg_matlab- img_avg_implemented))));
+    fprintf("average - Example: first element of both: %f and %f\n",img_avg_implemented(1,1), img_avg_matlab(1,1));
 end
