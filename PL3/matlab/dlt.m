0ba770c42ba6e8b66553c3a42dcff4484adf247f
JaplOfficial <53877277+JaplOfficial@users.noreply.github.com>
2023-04-11 16:38:34 +0100

    finding camera properties
    Computing camera matrix + intrinsic camera properties

--- PL3/matlab/dlt.m
+++ PL3/matlab/dlt.m
@@ -1,3 +1,12 @@
 function [P] = dlt(xy, XYZ)
 %computes DLT, xy and XYZ should be normalized before calling this function
+    A = [];
+    for i = 1 : size(xy, 1)
+        A(i, :) = [XYZ(:,i)', zeros(1, 4), -xy(1, i) * XYZ(:,i)'];
+        A(i + 1, :) = [zeros(1, 4), XYZ(:,i)', -xy(2, i) * XYZ(:,i)'];
+    end
 
+    [U, S, V] = svd(A);
+
+    P = reshape(V(:,end), [4, 3])';
+end
--- PL3/matlab/runDLT.m
+++ PL3/matlab/runDLT.m
@@ -1,15 +1,26 @@
 function [K, R, t, error] = runDLT(xy, XYZ)
 
 %normalize data points
-xy_normalized = [];
-XYZ_normalized = [];
+
+[xy_normalized, XYZ_normalized, T, U] = normalization(xy, XYZ)
 
 %compute DLT
-[P_normalized] = dlt(xy_normalized, XYZ_normalized);
+[P_normalized] = dlt(xy_normalized, XYZ_normalized)
 
 %denormalize camera matrix
 
+denormalized_camera = T \ P_normalized * U
+
 %factorize camera matrix in to K, R and t
+
+% To solve Pc = 0 we use SVD(M) and choose
+% the singular vector corresponding to the 
+% smallest singular value of V
+[~, ~, v] = svd(denormalized_camera);
+center = v(:, end);
+
+s = denormalized_camera(:, 1:3);
+[q, r] = qr(s);
 
 %compute reprojection error
 
