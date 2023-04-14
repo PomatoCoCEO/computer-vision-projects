9c002b1a45dc252463c21b4f4ab7bef710b0e7a2
PomatoCoCEO <paulomiguelc@gmail.com>
2023-04-14 10:46:53 +0100

    DEV: QR decomposition with the appropriate code

--- PL3/matlab/decomposeQR.m
+++ PL3/matlab/decomposeQR.m
@@ -3,12 +3,17 @@ function [ K, R, C ] = decomposeQR(P)
     % To solve Pc = 0 we use SVD(M) and choose
     % the singular vector corresponding to the 
     % smallest singular value of V
-    [~, ~, v] = svd(P');
-    C = v(:, end); % 3-element column vector - matrix was transposed so that the vectors were of 3 elements
-    
+    [~, s_vals, v] = svd(P);
+    C = v(:, end); 
+    % the point has homogenous coordinates, we need to normalize it and take the first 3 coordinates
+    C = C / C(4);
+    C = C(1:3);
+    % P' * C
     s = P(:, 1:3);
     s_minus_1 = inv(s);
-    [K_minus_1, R_minus_1] = qr(s_minus_1);
+    % C = - inv(s) * P(:,4);
+    [ R_minus_1, K_minus_1] = qr(s_minus_1);
+
     K = inv(K_minus_1);
     R = inv(R_minus_1);
 end
