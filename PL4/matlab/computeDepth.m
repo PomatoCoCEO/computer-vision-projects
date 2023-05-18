22a8e5f00b8fcfdf050a510ef47d6dc509c76f30
PomatoCoCEO <paulomiguelc@gmail.com>
2023-05-18 11:41:32 +0100

    DEV: disparity + depth map. Report!

--- PL4/matlab/computeDepth.m
+++ PL4/matlab/computeDepth.m
@@ -1,2 +1,9 @@
 function depthM = computeDepth(dispM, K1, K2, R1, R2, t1, t2, bits)
 % computeDepth creates a depth map from a disparity map (DISPM).
+    c1 = - (K1 * R1)\K1 * t1;
+    c2 = - (K2 * R2)\K2 * t2;
+    % baseline is the distance between optical centers
+    b = norm(c1-c2);
+    f = K1(1,1);
+    depthM = zeros(size(dispM));
+    depthM(dispM~=0) = (f*b)/dispM(dispM~=0);
--- PL4/matlab/computeDisparity.m
+++ PL4/matlab/computeDisparity.m
@@ -1,3 +1,34 @@
 function dispM = computeDisparity(im1, im2, maxDisp, windowSize)
 % computeDisparity creates a disparity map from a pair of rectified images im1 and
 %   im2, given the maximum disparity MAXDISP and the window size WINDOWSIZE.
+    sz = floor(windowSize / 2);
+    pd1 = padarray(im1, [sz sz] , 'replicate','both');
+    pd2 = padarray(im2, [sz sz] , 'replicate','both');
+    dispM = zeros(size(im1));
+    for y = 1 : height(im1)
+        for x = maxDisp+1:width(im1)
+            opt = 0;
+            opt_dist = inf;
+            w1 = pd1(y:y+windowSize-1, x:x+windowSize-1);
+            for k = 0 : maxDisp
+                w2 = pd2(y:y+windowSize-1, x-k:x-k+windowSize-1);
+                curr_dist = dist_window(w1,w2,"ssd");
+                if curr_dist < opt_dist
+                    opt_dist = curr_dist;
+                    opt = k;
+                end
+            end
+            dispM(y,x)=opt;
+        end
+    end
+end
+
+function dist = dist_window(w1, w2, criterion)
+    if strcmp(criterion,'ssd')
+        dist = sum((w1 - w2).^2,[1 2]);
+    elseif strcmp(criterion,'ncc')
+        dist = 1-normxcorr2(w1,w2);
+    else
+        error("Only 'ssd' and 'nnc' criteria acceptable");
+    end
+end
